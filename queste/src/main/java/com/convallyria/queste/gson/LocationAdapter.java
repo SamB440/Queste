d599b10943c48d92f0b4e5611d605d350e52474a
SamB440 <sam@islandearth.net>
2021-11-07 17:07:40 +0000

    Only apply pitch/yaw if needed in serialisation

--- queste/src/main/java/com/convallyria/queste/gson/LocationAdapter.java
+++ queste/src/main/java/com/convallyria/queste/gson/LocationAdapter.java
@@ -5,6 +5,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 import com.google.gson.reflect.TypeToken;
@@ -21,13 +22,21 @@ public class LocationAdapter implements JsonSerializer<Location>, JsonDeserializ
         this.gson = new GsonBuilder().create();
     }
 
+
     @Override
-    public Location deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context) {
-        return Location.deserialize(gson.fromJson(jsonElement, new TypeToken<Map<String, Object>>(){}.getType()));
+    public Location deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context) throws JsonParseException {
+        Map<String, Object> deserialised = gson.fromJson(jsonElement, new TypeToken<Map<String, Object>>(){}.getType());
+        if (!deserialised.containsKey("pitch")) deserialised.put("pitch", 0.0f);
+        if (!deserialised.containsKey("yaw")) deserialised.put("yaw", 0.0f);
+        return Location.deserialize(deserialised);
     }
 
     @Override
     public JsonElement serialize(Location location, Type type, JsonSerializationContext context) {
-        return gson.toJsonTree(location.serialize());
+        Map<String, Object> serialised = location.serialize();
+        if (Float.compare(location.getPitch(), 0.0f) == 0) serialised.remove("pitch");
+        if (Float.compare(location.getYaw(), 0.0f) == 0) serialised.remove("yaw");
+        return gson.toJsonTree(serialised);
     }
+
 }
